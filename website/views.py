a0632ef8bc62621b5d69886d9745220a62d1bdee
The-Broccoli <The-Broccoli@users.noreply.github.com>
2021-07-14 17:39:04 +0200

    adding content now works

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
-
+from .models import Post
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -8,7 +9,16 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('home.html', user=current_user)
 
-@views.route('/settings')
+@views.route('/settings', methods=['GET', 'POST'])
 @login_required
 def settings():
+    if request.method == 'POST':
+        description = request.form.get('description')
+        if len(description) < 1:
+            flash('description is too short!', category='error')
+        else:
+            new_post = Post(data=description, user_id=current_user.id)
+            db.session.add(new_post)
+            db.session.commit()
+            flash('Seite added!', category='success')
     return render_template('settings.html', user=current_user)
