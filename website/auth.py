78cbd9b635352daf31a00f7d2d915b488a1944d1
The-Broccoli <The-Broccoli@users.noreply.github.com>
2021-07-14 17:10:42 +0200

    add db // login and logout works

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,59 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from sqlalchemy.sql.functions import user
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.settings'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'logout'
+    logout_user()
+    return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
-    return render_template('sign_up.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Für diese Email gibt es schon ein Profil', category='error')
+        elif len(email) < 4:
+            flash('Email zu kurz', category='error')
+        elif len(first_name) < 2:
+            flash('Name zu kurz', category='error')
+        elif password1 != password2:
+            flash('Passwörter sind nicht gleich', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+            flash('Account wurde erstellt', category='success')
+            return redirect(url_for('views.settings'))
+
+    return render_template('sign_up.html', user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('home.html')
+
+@views.route('/settings')
+@login_required
+def settings():
+    return render_template('settings.html', user=current_user)
