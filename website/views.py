58e3b42a7ad1b10382e54a77531702e3ffdd67a4
The-Broccoli <The-Broccoli@users.noreply.github.com>
2021-07-15 11:48:06 +0200

    entries can now be deleted

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Post
 from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -22,3 +23,15 @@ def settings():
             db.session.commit()
             flash('Seite added!', category='success')
     return render_template('settings.html', user=current_user)
+
+@views.route('/delete-post', methods=['POST'])
+def delete_post():
+    post = json.loads(request.data)
+    postId = post['postId']
+    post = Post.query.get(postId)
+    if post:
+        if post.user_id == current_user.id:
+            db.session.delete(post)
+            db.session.commit()
+            return jsonify({})
+    return jsonify({})
